3f25c19a0f139c4f7978c43f0355c6825a67c2ee
Johann Wilfling <johann.wilfling@gmail.com>
2016-10-06 16:28:04 +0200

    Updated the example application.

--- Example/EBGaramondFontiOS/ViewController.swift
+++ Example/EBGaramondFontiOS/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+import EBGaramondFontiOS
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "This is EB Garamond 08 Regular."
+        label.font = UIFont.ebGaramond08RegularFontWithSize(17.0)
     }
 
     override func didReceiveMemoryWarning() {
