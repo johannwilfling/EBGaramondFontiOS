d443a63316333575d0845ff1c23b841b62229f01
Johann Wilfling <johann.wilfling@gmail.com>
2016-12-09 14:26:52 +0100

    Updated to Swift 3

--- EBGaramondFontiOS/Classes/EBGaramondFontiOS.swift
+++ EBGaramondFontiOS/Classes/EBGaramondFontiOS.swift
@@ -8,38 +8,38 @@
 
 public extension UIFont {
     
-    public class func ebGaramond08RegularFontWithSize(fontSize: CGFloat) -> UIFont {
+    public class func ebGaramond08RegularFontWithSize(_ fontSize: CGFloat) -> UIFont {
         FontLoader.loadFontIfNeeded()
         return UIFont(name: EBGaramondFonts.EBGaramond08Regular.rawValue, size: fontSize)!
     }
 
-    public class func ebGaramond08ItalicFontWithSize(fontSize: CGFloat) -> UIFont {
+    public class func ebGaramond08ItalicFontWithSize(_ fontSize: CGFloat) -> UIFont {
         FontLoader.loadFontIfNeeded()
         return UIFont(name: EBGaramondFonts.EBGaramond08Italic.rawValue, size: fontSize)!
     }
     
-    public class func ebGaramond12RegularFontWithSize(fontSize: CGFloat) -> UIFont {
+    public class func ebGaramond12RegularFontWithSize(_ fontSize: CGFloat) -> UIFont {
         FontLoader.loadFontIfNeeded()
         return UIFont(name: EBGaramondFonts.EBGaramond12Regular.rawValue, size: fontSize)!
     }
 
-    public class func ebGaramond12ItalicFontWithSize(fontSize: CGFloat) -> UIFont {
+    public class func ebGaramond12ItalicFontWithSize(_ fontSize: CGFloat) -> UIFont {
         FontLoader.loadFontIfNeeded()
         return UIFont(name: EBGaramondFonts.EBGaramond12Italic.rawValue, size: fontSize)!
     }
 
     // small caps
-    public class func ebGaramond12AllSCFontWithSize(fontSize: CGFloat) -> UIFont {
+    public class func ebGaramond12AllSCFontWithSize(_ fontSize: CGFloat) -> UIFont {
         FontLoader.loadFontIfNeeded()
         return UIFont(name: EBGaramondFonts.EBGaramond12AllSC.rawValue, size: fontSize)!
     }
 
-    public class func ebGaramondSC08RegularFontWithSize(fontSize: CGFloat) -> UIFont {
+    public class func ebGaramondSC08RegularFontWithSize(_ fontSize: CGFloat) -> UIFont {
         FontLoader.loadFontIfNeeded()
         return UIFont(name: EBGaramondFonts.EBGaramondSC08Regular.rawValue, size: fontSize)!
     }
 
-    public class func ebGaramondSC12RegularFontWithSize(fontSize: CGFloat) -> UIFont {
+    public class func ebGaramondSC12RegularFontWithSize(_ fontSize: CGFloat) -> UIFont {
         FontLoader.loadFontIfNeeded()
         return UIFont(name: EBGaramondFonts.EBGaramondSC12Regular.rawValue, size: fontSize)!
     }
@@ -67,41 +67,34 @@ private enum EBGaramondFonts: String {
     ]
 }
 
+
 private class FontLoader {
     
-    struct Static {
-        static var onceToken : dispatch_once_t = 0
-    }
-    
     static func loadFontIfNeeded() {
-        if (UIFont.fontNamesForFamilyName(EBGaramondFonts.FontFamilyName.rawValue).count == 0) {
+        if (UIFont.fontNames(forFamilyName: EBGaramondFonts.FontFamilyName.rawValue).count == 0) {
+            let bundle = Bundle(for: FontLoader.self)
+            var fontURL: URL!
+            let identifier = bundle.bundleIdentifier
             
-            dispatch_once(&Static.onceToken) {
-                let bundle = NSBundle(forClass: FontLoader.self)
-                var fontURL = NSURL()
-                let identifier = bundle.bundleIdentifier
+            for ebGaramondFont in EBGaramondFonts.allValues {
+                if identifier?.hasPrefix("org.cocoapods") == true {
+                    fontURL = bundle.url(forResource: ebGaramondFont.rawValue, withExtension: "otf", subdirectory: "EBGaramondFontiOS.bundle")
+                } else {
+                    fontURL = bundle.url(forResource: ebGaramondFont.rawValue, withExtension: "otf")
+                }
+                let data = try! Data(contentsOf: fontURL as URL)
+                let provider = CGDataProvider(data: data as CFData)
+                let font = CGFont(provider!)
                 
-                for ebGaramondFont in EBGaramondFonts.allValues {
-                    if identifier?.hasPrefix("org.cocoapods") == true {
-                        fontURL = bundle.URLForResource(ebGaramondFont.rawValue, withExtension: "otf", subdirectory: "EBGaramondFontiOS.bundle")!
-                    } else {
-                        fontURL = bundle.URLForResource(ebGaramondFont.rawValue, withExtension: "otf")!
-                    }
-                    let data = NSData(contentsOfURL: fontURL)!
+                var error: Unmanaged<CFError>?
+                if !CTFontManagerRegisterGraphicsFont(font, &error) {
                     
-                    let provider = CGDataProviderCreateWithCFData(data)
-                    let font = CGFontCreateWithDataProvider(provider)!
-                    
-                    var error: Unmanaged<CFError>?
-                    if !CTFontManagerRegisterGraphicsFont(font, &error) {
-                        
-                        let errorDescription: CFStringRef = CFErrorCopyDescription(error!.takeUnretainedValue())
-                        let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
-                        NSException(name: NSInternalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
-                    }
-                    
+                    let errorDescription: CFString = CFErrorCopyDescription(error!.takeUnretainedValue())
+                    let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
+                    NSException(name: NSExceptionName.internalInconsistencyException, reason: errorDescription as String, userInfo: [NSUnderlyingErrorKey: nsError]).raise()
                 }
             }
         }
     }
+    
 }
